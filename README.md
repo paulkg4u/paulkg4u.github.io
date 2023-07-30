59d676940317d6d85644136178e8ac464cb1de0d
Paul <paul@peers-solutions.com>
2023-07-31 00:12:39 +0200

    add vue

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# paulkg4u.github.io
+# Vue Tailwind Portfolio
+
+Fancy and minimal portfolio made on VUE JS and Tailwind CSS.
+
+## Features
+
+- Light/dark mode toggle
+- Fetched Projects from github repositories
+- Responsive
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/saileshrijal/vue-tailwind-portfolio
+```
+
+Go to the project directory
+
+```bash
+  cd vue-tailwind-portfolio
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
+Build the project
+
+```bash
+  npm run build
+```
+
+Build project is located on dist directory
+
+
+## 🚀 About Me
+
+I'm a full stack developer...
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="text-center">
+        <img src="../assets/imgs/dp.jpg"
+            class="w-[140px] h-[140px] sm:w-[140px] sm:h-[140px] mx-auto rounded-full shadow-md">
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+
+            I'm a Full-Stack Developer with keen interest in creating enterprise level web applications for various domains.
+
+            I use Python/Django in the backend and various JavaScript Frameworks in the front end.
+
+            I have also worked with tools/technologies like RestAPI, Postgres, Celery, WebRTC, Progressive Web Apps, etc.
+
+            I'm driven by passion for designing and developing end to end solutions, and managing its life-cycle.
+
+        </p>
+        <p class="text-center text-blue-600">#Django #Python #WebApplication</p>
+    </div>
+</template>
+<script setup>
+
+
+</script>

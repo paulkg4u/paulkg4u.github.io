a899622ab074752253675b516064a5d7ed3cc8f5
Paul K.George <paulkottalil20@gmail.com>
2024-01-17 17:39:08 +0100

    add peers iframe

---
+++ src/views/PeersView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-center">
+        <iframe src="https://devbackend.peers-solutions.com/learn/kira/start"></iframe>
+    </div>
+</template>
+<script setup>
+
+
+</script>
